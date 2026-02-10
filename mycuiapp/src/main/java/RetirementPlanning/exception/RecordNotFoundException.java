c3f726bc728ce0eebacba9dcf385079854b706f1
Jayaprakash A R <jayaprakash22072005@gmail.com>
2026-02-10 15:41:42 +0530

    Delete mycuiapp directory

--- mycuiapp/src/main/java/RetirementPlanning/exception/RecordNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package RetirementPlanning.exception;
-
-public class RecordNotFoundException extends Exception {
-    public RecordNotFoundException(String message) {
-        super(message);
-    }
-}
