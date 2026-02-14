a0f605358b73e04f52a376108a2d7a84408d3cb8
Jayaprakash A R <jayaprakash22072005@gmail.com>
2026-02-14 20:50:29 +0530

    Delete mycuiapp directory

--- mycuiapp/mycuiapp/src/main/java/RetirementPlanning/exception/RetirementException.java
+++
@@ -1,9 +0,0 @@
-package RetirementPlanning.exception;
-
-public class RetirementException extends Exception {
-    public RetirementException(String message) {
-        super(message);
-    }public RetirementException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
--- mycuiapp/src/main/java/RetirementPlanning/exception/RetirementException.java
+++
@@ -1,7 +0,0 @@
-package RetirementPlanning.exception;
-
-public class RetirementException extends Exception {
-    public RetirementException(String message) {
-        super(message);
-    }
-}
